a4760fbae23980499524748c88e29622b2391294
eograhix <97454093+eograhix@users.noreply.github.com>
2024-09-16 17:28:44 +0200

    Update fullscreen.js

--- js/fullscreen.js
+++ js/fullscreen.js
@@ -1,4 +1,17 @@
 var elem = document.getElementById("myvideo");
-if (elem.requestFullscreen) {
-  elem.requestFullscreen();
+
+if (elem) { // Check if the element exists
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { // Firefox
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { // Chrome, Safari, and Opera
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { // IE/Edge
+    elem.msRequestFullscreen();
+  } else {
+    console.log("Fullscreen API is not supported.");
+  }
+} else {
+  console.log("Element not found.");
 }
